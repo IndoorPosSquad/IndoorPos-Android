9ff3c691fa6a884e7cd42246be03eb93d6003de7
zhfuzzy <zhfuzzy@gmail.com>
2015-03-30 16:53:18 +0800

    bulk read is working

--- app/src/main/java/com/vetrm/indoorpos/DeviceMan.java
+++ app/src/main/java/com/vetrm/indoorpos/DeviceMan.java
@@ -2,7 +2,11 @@ package com.vetrm.indoorpos;
 
 import android.content.Context;
 import android.hardware.usb.UsbDevice;
+import android.hardware.usb.UsbDeviceConnection;
+import android.hardware.usb.UsbEndpoint;
+import android.hardware.usb.UsbInterface;
 import android.hardware.usb.UsbManager;
+import android.util.Log;
 
 import java.util.HashMap;
 
@@ -11,4 +15,44 @@ import java.util.HashMap;
  */
 public class DeviceMan {
     // 0483:5741
+    private UsbDevice device;
+    private UsbManager manager;
+    private UsbDeviceConnection conn;
+    private UsbInterface intf;
+    private UsbEndpoint ep;
+
+    private void log(Object obj) {
+        final String DBG_TAG = "DeviceMan";
+        Log.d(DBG_TAG, "" + obj);
+    }
+
+    public DeviceMan(UsbDevice dev, UsbManager man) {
+        device = dev;
+        manager = man;
+
+        conn = manager.openDevice(device);
+
+        intf = device.getInterface(0);
+        ep = intf.getEndpoint(2);
+
+        log(conn.claimInterface(intf, true));
+    }
+
+    public void read() {
+        final int len = 64;
+
+        byte[] content = new byte[len];
+
+        log("------------------------");
+        for (int i = 0; i < 5; i++) {
+            System.out.println(conn.bulkTransfer(ep, content, content.length, 20));
+
+            for (int j = 0; j < len; j++) {
+                System.out.print(content[j]);
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+        log("------------------------");
+    }
 }
--- app/src/main/java/com/vetrm/indoorpos/Main.java
+++ app/src/main/java/com/vetrm/indoorpos/Main.java
@@ -7,40 +7,56 @@ import android.hardware.usb.UsbDeviceConnection;
 import android.hardware.usb.UsbEndpoint;
 import android.hardware.usb.UsbInterface;
 import android.hardware.usb.UsbManager;
+import android.hardware.usb.UsbRequest;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
-import java.util.HashMap;
+import java.nio.ByteBuffer;
+
+import app.akexorcist.bluetotohspp.library.BluetoothSPP;
+import app.akexorcist.bluetotohspp.library.BluetoothState;
+import app.akexorcist.bluetotohspp.library.DeviceList;
 
 public class Main extends ActionBarActivity {
+    private UsbDevice device;
+    private UsbManager manager;
+    /*
+    private UsbDeviceConnection conn;
+    private UsbInterface intf;
+    private UsbEndpoint ep;
+    */
+
+    private DeviceMan devman;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        UsbDevice device = getIntent().getParcelableExtra(UsbManager.EXTRA_DEVICE);
-        UsbManager manager = (UsbManager) getSystemService(Context.USB_SERVICE);
+        device = getIntent().getParcelableExtra(UsbManager.EXTRA_DEVICE);
+        manager = (UsbManager) getSystemService(Context.USB_SERVICE);
+        devman = new DeviceMan(device, manager);
+        /*
+        conn = manager.openDevice(device);
+
+        intf = device.getInterface(0);
+        ep = intf.getEndpoint(2);
+
+        System.out.println(conn.claimInterface(intf, true));
+        */
+
         //HashMap<String, UsbDevice> deviceList = manager.getDeviceList();
         //UsbDevice device = (UsbDevice) deviceList.values().toArray()[0];
         //System.out.println("Printing USBinfo:");
-        System.out.println(device);
+        //System.out.println(device);
         //System.out.println(device.getVendorId()); // dec 1155 -> hex 0483
         //System.out.println(device.getProductId()); // dec 22337 -> hex 5741
         //System.out.println(deviceList.values().toArray()[0]);
-        //UsbInterface intf = device.getInterface(0);
-        //UsbEndpoint ep = intf.getEndpoint(0);
 
-        byte[] content = new byte[64];
-
-        //UsbDeviceConnection conn = manager.openDevice(device);
-        //System.out.println(conn.claimInterface(intf, true));
-        // System.out.println(conn.bulkTransfer(ep, content, content.length, 1));
-
-        System.out.println(content.toString());
     }
 
 
@@ -68,11 +84,66 @@ public class Main extends ActionBarActivity {
 
     public void showGraphics(View view) {
         Intent intent = new Intent(this, Visualization.class);
-
         startActivity(intent);
     }
 
-    public void listUSB() {
+    public void read(View view) {
+        devman.read();
+        //System.out.println(ep.getDirection());
+        //System.out.println(UsbConstants.USB_DIR_IN); // => 128
+        //Thread read = new Thread(this);
+        //read.start();
+
+        /*
+        final int len = 64;
+
+        byte[] content = new byte[len];
+
+        System.out.println("------------------------");
+        for (int i = 0; i < 5; i++) {
+            System.out.println(conn.bulkTransfer(ep, content, content.length, 20));
+
+            for (int j = 0; j < len; j++) {
+                System.out.print(content[j]);
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+        System.out.println("------------------------");
+        */
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+    }
+
+    /*
+    @Override
+    public void run() {
+        ByteBuffer bf = ByteBuffer.allocate(64);
+        UsbRequest request = new UsbRequest();
+        request.initialize(conn, device.getInterface(0).getEndpoint(0));
+
+
+        System.out.println("1");
+        while (true) {
+            request.queue(bf, 1);
+            System.out.println("2");
+            if (conn.requestWait() == request) {
+                byte rec = bf.get(0);
+                System.out.println(rec);
+
+                try {
+                    Thread.sleep(200);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                break;
+            }
+
+        }
 
     }
+    */
 }
